0d4f3ffd0f2c7487805cc1f04d690df68d971275
Rajesh <112327214+Raj-in-Git@users.noreply.github.com>
2025-10-21 14:03:56 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: "/e-plantShopping",
   plugins: [react()],
-});
+  base: '/e-plantShopping/',   // ← use your repo name with leading/trailing slash
+})
+
